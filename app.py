47631ff85a1e915271b21da19585550afedead9b
heflinstephenraj <heflinstephen03@gmail.com>
2021-05-29 17:26:14 +0530

    Added Bar Chart for Recoveries

--- app.py
+++ app.py
@@ -24,12 +24,16 @@ def fetch_data(url):
 #get the data
 covid_data = fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 covid_death_data =fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+covid_recovered_data =fetch_data("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
 
 #data processing
 last_updated__death_date=covid_death_data.columns[-1]
 counrty_wise_death_cases = covid_death_data.groupby('Country/Region').agg({ last_updated__death_date:'sum'}).reset_index().sort_values(last_updated__death_date,ascending=False)
 last_updated_date=covid_data.columns[-1]
 counrty_wise_confirmed_cases = covid_data.groupby('Country/Region').agg({ last_updated_date:'sum'}).reset_index().sort_values(last_updated_date,ascending=False)
+last_updated_date_recovered=covid_recovered_data.columns[-1]
+counrty_wise_recovered_cases = covid_recovered_data.groupby('Country/Region').agg({ last_updated_date_recovered:'sum'}).reset_index().sort_values(last_updated_date_recovered,ascending=False)
+
 
 #data info
 st.write(f"This data is obtained from [Johns Hopkins University.](https://github.com/CSSEGISandData/COVID-19) Last updated on {last_updated_date}")
@@ -55,4 +59,5 @@ def bar_chart_countries(data,title,xaxis,yaxis,):
 #plot the bar chart
 col1,col2 = st.beta_columns(2)
 col1.plotly_chart(bar_chart_countries(counrty_wise_confirmed_cases,title="Total Confirmed Cases",xaxis="Countries",yaxis="No. of people"))
-col2.plotly_chart(bar_chart_countries(counrty_wise_death_cases,title="Total Death Cases",xaxis="Countries",yaxis="No. of people"))
+col2.plotly_chart(bar_chart_countries(counrty_wise_death_cases,title="Total Deaths",xaxis="Countries",yaxis="No. of people"))
+col1.plotly_chart(bar_chart_countries(counrty_wise_recovered_cases,title="Total Recoveries",xaxis="Countries",yaxis="No. of people"))

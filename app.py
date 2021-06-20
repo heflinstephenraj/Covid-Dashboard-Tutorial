5480ee650917627a852c564e725afe7323c916db
heflinstephenraj <heflinstephen03@gmail.com>
2021-06-20 12:43:07 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.set_page_config(page_title="Covid Dashboard", page_icon="🕸", layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title="Covid Dashboard", page_icon="🕸", layout='wide')
 
 st.header("Covid Dashboard")
 st.subheader("Developed with ❤ by [Heflin Stephen Raj](https://www.heflin.dev/)")

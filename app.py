8be10eab157d2f2f5ada37c0e032f6d8616a0507
heflinstephenraj <heflinstephen03@gmail.com>
2021-07-18 17:50:07 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 
 st.set_page_config(page_title="Covid Dashboard", page_icon="🕸", layout='wide')
